06edd0caea945a6a32e20722609f87705ebbc122
Jorge <jvinuezam@gmail.com>
2019-09-10 08:30:18 -0500

    Mi aplicación Django, primer commit

---
+++ mysite/blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+
+class User(models.Model):
+    author = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    addres = models.TextField(blank=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
