9cd9b128b51d8c08632c815907d55b718645cb5a
Jorge <jvinuezam@gmail.com>
2019-09-11 22:40:19 -0500

    Templates modificados para mostrar post desde base de datos

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -10,7 +10,7 @@ class User(models.Model):
 
 
 class Post(models.Model):
-    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+# el signo . indica el directorio actual o la aplicación actual
+from . models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
